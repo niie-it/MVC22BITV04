6e43fbb9e7933a44623d177ec1e9fb49231c329e
Hien Luong Tran Hy <hyhien@gmail.com>
2025-07-04 16:17:50 +0700

    Area

---
+++ Buoi05/DemoLayout/Areas/Admin/Controllers/LoaiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLayout.Areas.Admin.Controllers
+{
+	[Area("admin")]
+	public class LoaiController : Controller
+	{
+		// host/admin/Loai/Index
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
